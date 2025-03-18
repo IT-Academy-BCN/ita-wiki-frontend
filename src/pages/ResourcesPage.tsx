d1490432a1ed7ae9951003247b91241e4853ce3b
JuanDNJ <juandevnjv@gmail.com>
2025-03-18 21:50:04 +0100

    fix: Adding MainContent, creating new sections

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -5,13 +5,18 @@ import { ListResources } from "../components/resources/ListResources";
 import { getResources } from "../api/endPointResources";
 import { categories } from "../data/categories";
 import moock from "../moock/resources.json";
+import MainContent from "../Layout/MainContent";
+import { getPersonalResources } from "../api/userApi";
+import { useCtxUser } from "../hooks/useCtxUser";
+import { ListMyResources } from "../components/resources/ListMyResources";
 
 const ResourcesPage: FC = () => {
   const { category } = useParams();
   const navigate = useNavigate();
   const [apiResources, setApiResources] = useState<IntResource[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const { user } = useCtxUser();
+  const personalResources = getPersonalResources(user, apiResources);
   useEffect(() => {
     if (!category) {
       navigate(`/resources/${categories[0]}`);
@@ -39,16 +44,26 @@ const ResourcesPage: FC = () => {
   }, []);
 
   return (
-    <>
-      {isLoading ? (
-        <div>Obteniendo los recursos...</div>
-      ) : (
-        <ListResources
-          resources={apiResources}
-          category={category as keyof typeof categories | undefined}
-        />
-      )}
-    </>
+    <MainContent>
+      <section className="flex w-full bg-white rounded-2xl px-4 py-6 sm:px-6 lg:pl-8 xl:shrink-0 xl:pl-6">
+        {isLoading ? (
+          <div>Obteniendo los recursos...</div>
+        ) : (
+          <ListResources
+            resources={apiResources}
+            category={category as keyof typeof categories | undefined}
+          />
+        )}
+      </section>
+      <section className="flex flex-col w-full">
+        <article className="bg-white sm:rounded-xl px-4 py-6 sm:px-6 lg:pl-8 xl:shrink-0 xl:pl-6">
+          <h3 className="text-[22px] font-bold">Lista de lectura</h3>
+        </article>
+        {user && personalResources.length > 0 && (
+          <ListMyResources myResources={personalResources} />
+        )}
+      </section>
+    </MainContent>
   );
 };
 
