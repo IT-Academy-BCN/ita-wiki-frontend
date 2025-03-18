aba16516b666c0a5e2cb4084d59f20e545e7129c
JuanDNJ <juandevnjv@gmail.com>
2025-03-18 13:43:19 +0100

    fix: Updating styles and the icon directory

--- src/Layout/AsideComponent.tsx
+++ src/Layout/AsideComponent.tsx
@@ -1,12 +1,13 @@
-import node from "../assets/logo-node 1.svg";
-import react from "../assets/react.svg";
-import angular from "../assets/angular.svg";
-import javascript from "../assets/javascript.svg";
-import java from "../assets/logo-java 1.svg";
-import php from "../assets/logo-php 1.svg";
-import dataScience from "../assets/data-science.svg";
-import bbdd from "../assets/logo-bbdd 1.svg";
+import node from "../assets/svg/logo-node 1.svg";
+import react from "../assets/svg/react.svg";
+import angular from "../assets/svg/angular.svg";
+import javascript from "../assets/svg/javascript.svg";
+import java from "../assets/svg/logo-java 1.svg";
+import php from "../assets/svg/logo-php 1.svg";
+import dataScience from "../assets/svg/data-science.svg";
+import bbdd from "../assets/svg/logo-bbdd 1.svg";
 import { Link } from "react-router";
+import logoItAcademy from "../assets/svg/LogoItAcademy.svg";
 
 const AsideComponent = () => {
   const asideContent = [
@@ -21,8 +22,13 @@ const AsideComponent = () => {
   ];
 
   return (
-    <aside className="flex p-6 items-center justify-center lg:block lg:w-80 lg:h-screen lg:max-h-[calc(100vh-90px)] lg:mt-36">
-      <ul className="space-y-6">
+    <aside className="sidebar">
+      <section className="col-1 flex items-center pt-[21px] pl-[23px]">
+        <Link to="/">
+          <img src={logoItAcademy} alt="logo" width={"116px"} height={29} />
+        </Link>
+      </section>
+      <ul className="space-y-6 pt-[21px] pl-[23px] flex flex-col justify-center">
         {asideContent.map((item, index) => (
           <li key={index} className="flex items-center space-x-3">
             <img src={item.icon} alt={item.label} className="w-6 h-6" />
