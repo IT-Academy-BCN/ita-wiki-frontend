1bf56b9d8539e2aea5f8514f3f6998c058392e27
JuanDNJ <juandevnjv@gmail.com>
2025-03-18 20:47:40 +0100

    fix: Adding search

--- src/components/resources/ListResources.tsx
+++ src/components/resources/ListResources.tsx
@@ -6,6 +6,7 @@ import { categories } from "../../data/categories";
 import { themes } from "../../data/themes";
 import { resourceTypes } from "../../data/resourceTypes";
 import { useResourceFilter } from "../../hooks/useResourceFilter";
+import searchIcon from "../../assets/svg/search.svg";
 
 interface ListResourceProps {
   resources: IntResource[];
@@ -52,9 +53,33 @@ export const ListResources: FC<ListResourceProps> = ({
           </section>
           <section className="lg:flex-1 px-4 py-6 lg:pl-8 xl:pl-6">
             <article className="flex justify-between items-center">
-              <h2 className="text-[26px] font-bold">
-                Recursos {String(category) || ""}
-              </h2>
+              <header className="w-full flex items-center gap-2 justify-between">
+                <h2 className="text-[26px] font-bold">
+                  Recursos {String(category) || ""}
+                </h2>
+                <article className="relative inline-flex cursor-pointer">
+                  <input
+                    type="text"
+                    placeholder="Buscar recurso"
+                    className="bg-white pl-10 pr-4 py-2 border border-white font-semibold rounded-lg
+                   focus:outline-none focus:ring-2 focus:ring-[#808080]"
+                  />
+                  <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#808080]">
+                    <svg
+                      xmlns={searchIcon}
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.9 14.32a8 8 0 111.42-1.42l4.83 4.83a1 1 0 01-1.42 1.42l-4.83-4.83zM8 14a6 6 0 100-12 6 6 0 000 12z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </article>
+              </header>
               {/* Filter Button (Mobile only) */}
               <button
                 className="sm:hidden bg-[#B91879] text-white px-4 py-2 rounded-md flex items-center gap-2"
