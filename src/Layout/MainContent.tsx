ebeec890d939fee0e47808614f535816c0fb75ef
JuanDNJ <juandevnjv@gmail.com>
2025-03-18 20:44:21 +0100

    fix: Add  gap

--- src/Layout/MainContent.tsx
+++ src/Layout/MainContent.tsx
@@ -3,7 +3,7 @@ import { TypChildren } from "../types";
 
 const MainContent: FC<TypChildren> = ({ children }) => {
 
-  return <main className="main__content grid pr-6 max-h-[815px] overflow-x-hidden overflow-y-auto">
+  return <main className="main__content grid gap-6 pr-6 max-h-[815px] overflow-x-hidden overflow-y-auto">
     {children}
   </main>
 }
