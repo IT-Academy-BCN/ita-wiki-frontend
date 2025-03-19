1dcd47107c50e793f15bc4ff0f4281ed57f4a974
JuanDNJ <juandevnjv@gmail.com>
2025-03-19 12:53:07 +0100

    fix: Provisionally using the created hooks

--- src/Layout/AsideComponent.tsx
+++ src/Layout/AsideComponent.tsx
@@ -8,8 +8,12 @@ import dataScience from "../assets/svg/data-science.svg";
 import bbdd from "../assets/svg/logo-bbdd 1.svg";
 import { Link } from "react-router";
 import logoItAcademy from "../assets/svg/LogoItAcademy.svg";
-
+import close from "../assets/svg/close.svg";
+import { useCtxUser } from "../hooks/useCtxUser";
 const AsideComponent = () => {
+
+  const { isTablet, isMobile, toggleMainMenu, isOpenMainMenu } = useCtxUser()
+
   const asideContent = [
     { icon: node, label: "Node" },
     { icon: react, label: "React" },
@@ -22,11 +26,16 @@ const AsideComponent = () => {
   ];
 
   return (
-    <aside className="sidebar">
+    <aside className="sidebar grid grid-cols-3 bg-[#EBEBEB]" style={{
+      left: isOpenMainMenu ? "0px" : '-100%',
+    }}>
       <section className="col-1 flex items-center pt-[21px] pl-[23px]">
-        <Link to="/">
-          <img src={logoItAcademy} alt="logo" width={"116px"} height={29} />
-        </Link>
+        {isTablet || isMobile ? <button onClick={toggleMainMenu} type="button">
+          <img src={close} alt="logo" width={"32"} height={32} />
+        </button> :
+          <Link to="/">
+            <img src={logoItAcademy} alt="logo" width={"116px"} height={29} />
+          </Link>}
       </section>
       <ul className="space-y-6 pt-[21px] pl-[23px] flex flex-col justify-center">
         {asideContent.map((item, index) => (
