d2c30b5e4831d6788f33291ec365e67778f26b7c
JuanDNJ <juandevnjv@gmail.com>
2025-03-19 13:35:03 +0100

    fix: Move index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import "./assets/css/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import UserCtxProvider from "./context/UserCtxProvider.tsx";
 import { BrowserRouter } from "react-router";
