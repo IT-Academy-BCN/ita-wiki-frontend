8a38da41f47dcbcab03f0851f70f18e0b152b37a
JuanDNJ <juandevnjv@gmail.com>
2025-03-19 12:59:02 +0100

    fix: Provisionally using the create kooks

--- src/Layout/HeaderComponent.tsx
+++ src/Layout/HeaderComponent.tsx
@@ -8,6 +8,7 @@ import { Modal } from "../components/Modal/Modal";
 import { useCtxUser } from "../hooks/useCtxUser";
 import { useState } from "react";
 import GitHubLogin from "../components/github-login/GitHubLogin";
+import menubars from "../assets/svg/Vector-7.svg";
 
 const HeaderComponent = () => {
   const { user, signIn } = useCtxUser();
@@ -39,74 +40,80 @@ const HeaderComponent = () => {
     setIsChecked(!isChecked);
     setLoginError(false);
   };
-
+  const { isTablet, isMobile, toggleMainMenu } = useCtxUser()
   return (
-    <header className="main__header hidden lg:grid bg-[#ebebeb] justify-end gap-6 items-center pr-6">
-      <section className="flex justify-end">
-        <article className="relative inline-flex cursor-pointer">
-          <input
-            type="text"
-            placeholder="Buscar recurso"
-            className="bg-white pl-10 pr-4 py-2 border border-white font-semibold text-base rounded-lg
-                   focus:outline-none focus:ring-2 focus:ring-[#808080]"
-          />
-          <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#808080]">
-            <svg
-              xmlns={searchIcon}
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.9 14.32a8 8 0 111.42-1.42l4.83 4.83a1 1 0 01-1.42 1.42l-4.83-4.83zM8 14a6 6 0 100-12 6 6 0 000 12z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        </article>
-      </section>
-      <section className="flex gap-4 justify-end items-center">
-        {user && (
-          <ButtonComponent
-            icon={addIcon}
-            variant="icon"
-            onClick={goToResourcesPage}
-          />
-        )}
-        <select
-          title="lang"
-          className="bg-white py-2 px-4 text-[#808080] rounded-lg border border-white shadow-sm focus:outline-none focus:ring-2 focus:ring-[#808080] focus:border-transparent"
-        >
-          <option>ES</option>
-          <option>EN</option>
-        </select>
-        <ButtonComponent icon={settingsIcon} variant="icon" />
-        <ButtonComponent icon={userIcon} variant="icon" onClick={openModal} />
-        {isModalOpen && (
-          <Modal closeModal={closeModal} title="Inicio sesión">
-            <GitHubLogin onClick={handleSignIn} />
-            <label htmlFor="terms" className="block mt-8">
+    <header className="main__header grid bg-[#ebebeb] justify-end gap-6 items-center pr-6">
+      {isTablet || isMobile ? <button onClick={toggleMainMenu} type="button">
+        <img src={menubars} alt="logo" width={"32"} height={32} />
+      </button> :
+        <>
+          <section className="flex justify-end">
+            <article className="relative inline-flex cursor-pointer">
               <input
-                name="terms"
-                id="terms"
-                type="checkbox"
-                onChange={handleCheckboxChange}
-                checked={isChecked}
-              ></input>
-              Acepto términos legales
-            </label>
-            {loginError && (
-              <div className="text-red-500 text-sm mt-2">
-                <div className="text-red-500 text-sm mt-2 text-center">
-                  Lo sentimos, no se ha podido iniciar sesión,
-                  <br /> contacte con el administrador.
-                </div>
+                type="text"
+                placeholder="Buscar recurso"
+                className="bg-white pl-10 pr-4 py-2 border border-white font-semibold text-base rounded-lg
+                   focus:outline-none focus:ring-2 focus:ring-[#808080]"
+              />
+              <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#808080]">
+                <svg
+                  xmlns={searchIcon}
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.9 14.32a8 8 0 111.42-1.42l4.83 4.83a1 1 0 01-1.42 1.42l-4.83-4.83zM8 14a6 6 0 100-12 6 6 0 000 12z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </div>
+            </article>
+          </section>
+          <section className="flex gap-4 justify-end items-center">
+            {user && (
+              <ButtonComponent
+                icon={addIcon}
+                variant="icon"
+                onClick={goToResourcesPage}
+              />
             )}
-          </Modal>
-        )}
-      </section>
+            <select
+              title="lang"
+              className="bg-white py-2 px-4 text-[#808080] rounded-lg border border-white shadow-sm focus:outline-none focus:ring-2 focus:ring-[#808080] focus:border-transparent"
+            >
+              <option>ES</option>
+              <option>EN</option>
+            </select>
+            <ButtonComponent icon={settingsIcon} variant="icon" />
+            <ButtonComponent icon={userIcon} variant="icon" onClick={openModal} />
+            {isModalOpen && (
+              <Modal closeModal={closeModal} title="Inicio sesión">
+                <GitHubLogin onClick={handleSignIn} />
+                <label htmlFor="terms" className="block mt-8">
+                  <input
+                    name="terms"
+                    id="terms"
+                    type="checkbox"
+                    onChange={handleCheckboxChange}
+                    checked={isChecked}
+                  ></input>
+                  Acepto términos legales
+                </label>
+                {loginError && (
+                  <div className="text-red-500 text-sm mt-2">
+                    <div className="text-red-500 text-sm mt-2 text-center">
+                      Lo sentimos, no se ha podido iniciar sesión,
+                      <br /> contacte con el administrador.
+                    </div>
+                  </div>
+                )}
+              </Modal>
+            )}
+          </section>
+        </>
+      }
     </header>
   );
 };
