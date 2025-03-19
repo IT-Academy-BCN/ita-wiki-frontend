7bc771262d6818c219e696f5e5914aa681b19f96
JuanDNJ <juandevnjv@gmail.com>
2025-03-19 12:51:10 +0100

    fix: Add the hook to the global context

--- src/context/UserCtxProvider.tsx
+++ src/context/UserCtxProvider.tsx
@@ -3,13 +3,14 @@ import { FC, ReactNode } from "react";
 import { useUser } from "../hooks/useUser";
 import CtxUser, { PropsContext } from ".";
 import { useResize } from "../hooks/useResize";
+import { useMainMenu } from "../hooks/useMainMenu";
 
 interface Props {
   children: ReactNode;
 }
 
 const UserCtxProvider: FC<Props> = ({ children }) => {
-  const value = { ...useUser(), ...useResize() } as PropsContext;
+  const value = { ...useUser(), ...useResize(), ...useMainMenu() } as PropsContext;
   return <CtxUser.Provider value={value}>{children}</CtxUser.Provider>;
 };
 
