4019385cd4fab373bc8b7f865a19b21aaa290f16
JuanDNJ <juandevnjv@gmail.com>
2025-03-19 21:44:27 +0100

    fix: Using GlobalCtxProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,33 +3,36 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import UserCtxProvider from "./context/UserCtxProvider.tsx";
+import GlobalCtxProvider from "./context/GlobalCtxProvider.tsx";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
-  <UserCtxProvider>
-    <StrictMode>
-      <BrowserRouter>
-        <Toaster
-          richColors
-          toastOptions={{
-            style: {
-              padding: "2rem",
-              fontSize: "1rem",
-            },
-          }}
-        />
-        <App />
-        <Toaster
-          richColors
-          toastOptions={{
-            style: {
-              padding: "2rem",
-              fontSize: "1rem",
-            },
-          }}
-        />
-      </BrowserRouter>
-    </StrictMode>
-  </UserCtxProvider>,
+  <GlobalCtxProvider>
+    <UserCtxProvider>
+      <StrictMode>
+        <BrowserRouter>
+          <Toaster
+            richColors
+            toastOptions={{
+              style: {
+                padding: "2rem",
+                fontSize: "1rem",
+              },
+            }}
+          />
+          <App />
+          <Toaster
+            richColors
+            toastOptions={{
+              style: {
+                padding: "2rem",
+                fontSize: "1rem",
+              },
+            }}
+          />
+        </BrowserRouter>
+      </StrictMode>
+    </UserCtxProvider>
+  </GlobalCtxProvider>,
 );
