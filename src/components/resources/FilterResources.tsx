a40983bbebaa11ef8fd1ea570b14365d9ce7c3dc
JuanDNJ <juandevnjv@gmail.com>
2025-03-19 22:39:40 +0100

    fix: Adapting titles to mobile

--- src/components/resources/FilterResources.tsx
+++ src/components/resources/FilterResources.tsx
@@ -30,8 +30,8 @@ export const FilterResources: FC<FilterResourcesProps> = ({
       setSelectedResourceTypes(
         selectedResourceTypes.includes(resourceType)
           ? selectedResourceTypes.filter(
-              (rType: string) => rType !== resourceType,
-            )
+            (rType: string) => rType !== resourceType,
+          )
           : [...selectedResourceTypes, resourceType],
       );
     }
@@ -62,7 +62,7 @@ export const FilterResources: FC<FilterResourcesProps> = ({
   return (
     <div className="mt-6">
       <div className="mb-6">
-        <h3 className="text-lg font-bold mb-3">Temas</h3>
+        <h3 className="text-lg md:text-[26px] font-bold mb-3">Temas</h3>
         {themes.map((theme) => (
           <label
             key={theme}
@@ -77,9 +77,8 @@ export const FilterResources: FC<FilterResourcesProps> = ({
               className="hidden"
             />
             <div
-              className={`w-4 h-4 border-2 rounded-full flex items-center justify-center ${
-                selectedTheme === theme ? "border-[#B91879]" : "border-gray-400"
-              }`}
+              className={`w-4 h-4 border-2 rounded-full flex items-center justify-center ${selectedTheme === theme ? "border-[#B91879]" : "border-gray-400"
+                }`}
             >
               {selectedTheme === theme && (
                 <div className="w-2.5 h-2.5 bg-[#B91879] rounded-full"></div>
@@ -104,11 +103,10 @@ export const FilterResources: FC<FilterResourcesProps> = ({
               className="hidden"
             />
             <div
-              className={`w-5 h-5 flex items-center justify-center rounded border ${
-                selectedResourceTypes.includes(resourceType)
-                  ? "bg-[#B91879] border-[#B91879]"
-                  : "border-gray-400"
-              }`}
+              className={`w-5 h-5 flex items-center justify-center rounded border ${selectedResourceTypes.includes(resourceType)
+                ? "bg-[#B91879] border-[#B91879]"
+                : "border-gray-400"
+                }`}
             >
               {selectedResourceTypes.includes(resourceType) && (
                 <svg
