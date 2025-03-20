e04d56dfb3c106937a7061965d650a27b8600c0f
JuanDNJ <juandevnjv@gmail.com>
2025-03-20 23:54:51 +0100

    feature: Creating Main

---
+++ src/Layout/Main.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactNode } from "react"
+import layoutCSS from "./css/layout.module.css"
+export const Main: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <main className={`${layoutCSS.main} lg:p-6`} >
+      {children}
+    </main>
+  )
+}
