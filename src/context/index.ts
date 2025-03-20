538685fa4a757d21be933a3ef47952b0c86d9160
JuanDNJ <juandevnjv@gmail.com>
2025-03-20 23:58:55 +0100

    refactor: types

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,16 +1,5 @@
 import { createContext } from "react";
-import { IntUser } from "../types";
-import { IUseResize } from "../hooks/useResize";
-import { IntUseMainMenu } from "../hooks/useMainMenu";
-interface PropsContextUser {
-  user: IntUser;
-  saveUser: (user: IntUser) => void;
-  signIn: () => void;
-  signOut: () => void;
-  error: string | null;
-  setError: (error: string | null) => void;
-}
-interface PropsContexGLobal extends IUseResize, IntUseMainMenu {}
+import { PropsContexGLobal, PropsContextUser } from "./typesCtx";
 
 const CtxUser = createContext<PropsContextUser | null>(null);
 
@@ -18,4 +7,4 @@ const CtxGLobal = createContext<PropsContexGLobal | null>(null);
 
 export { CtxUser, CtxGLobal };
 
-export type { PropsContextUser, PropsContexGLobal };
+
