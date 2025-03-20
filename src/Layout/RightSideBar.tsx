c0fd492f490da00f4b7f6e0e8e81c727f5686664
JuanDNJ <juandevnjv@gmail.com>
2025-03-21 00:00:56 +0100

    feature: Creating RightSideBar

---
+++ src/Layout/RightSideBar.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react"
+import { useUserCtx } from "../hooks/useUserCtx";
+import { getPersonalResources } from "../api/userApi";
+import layoutCSS from "./css/layout.module.css";
+import { IntResource } from "../types";
+import { ListMyResources } from "../components/resources/ListMyResources";
+
+interface RightSideBarProps {
+  resources: IntResource[];
+}
+
+const RightSideBar: FC<RightSideBarProps> = ({ resources }) => {
+  const { user } = useUserCtx();
+  const personalResources = getPersonalResources(user, resources);
+
+  return (
+    <section className={`${layoutCSS.rightSideBar} flex flex-col lg:pr-6`}>
+      <article className="bg-white sm:rounded-xl px-4 py-6 ">
+        <h3 className="text-[22px] font-bold">Lista de lectura</h3>
+      </article>
+      {user && personalResources.length > 0 && (
+        <ListMyResources myResources={personalResources} />
+      )}
+    </section>
+  )
+}
+
+export default RightSideBar
