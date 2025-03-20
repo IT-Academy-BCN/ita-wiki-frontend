7999c9eac691877f4baaf4929e84e747a656c9e7
JuanDNJ <juandevnjv@gmail.com>
2025-03-21 00:00:25 +0100

    feature: Creating typesCtx

---
+++ src/context/typesCtx.ts
@@ -0,0 +1,13 @@
+import { IntUseMainMenu } from "../hooks/useMainMenu";
+import { IntUser } from "../types";
+import { IUseResize } from "../hooks/useResize";
+
+export interface PropsContextUser {
+  user: IntUser;
+  saveUser: (user: IntUser) => void;
+  signIn: () => void;
+  signOut: () => void;
+  error: string | null;
+  setError: (error: string | null) => void;
+}
+export interface PropsContexGLobal extends IUseResize, IntUseMainMenu { }
