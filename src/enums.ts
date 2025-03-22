e0da0bbf67fb3f7431b0973ea75351f8bd1adbaf
JuanDNJ <juandevnjv@gmail.com>
2025-03-22 12:53:27 +0100

    feature: Create enums

---
+++ src/enums.ts
@@ -0,0 +1,31 @@
+export enum EnuResourceThemes {
+  All = "Todos",
+  Eventos = "Eventos",
+  RenderizadoCondicional = "Renderizado condicional",
+  Components = "Components",
+  Listas = "Listas",
+  UseStateAndUseEffect = "UseState & UseEffect",
+  Java = "Java",
+  ReactRouter = "React Router",
+  DataScience = "Data Science",
+  Estilos = "Estilos",
+}
+
+export enum EnuResourceTypes {
+  ALL = "Todos",
+  Video = "Video",
+  Cursos = "Cursos",
+  Blog = "Blog",
+}
+
+export enum EnuResourcesCategories {
+  ALL = "Todos",
+  Node = "Node",
+  React = "React",
+  Angular = "Angular",
+  Javascript = "Javascript",
+  Java = "Java",
+  FullstackPHP = "Fullstack PHP",
+  DataScience = "Data Science",
+  BBDD = "BBDD",
+}
