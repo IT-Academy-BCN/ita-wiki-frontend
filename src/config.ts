bcec2c9d26f2bc94aaa31015abed5c8f99f1ee2f
JuanDNJ <juandevnjv@gmail.com>
2025-03-22 12:54:18 +0100

    refactor: Add ICONS

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,13 @@
 const API_URL = import.meta.env.VITE_API_URL;
-
+import node from "./assets/svg/node.svg";
+import react from "./assets/svg/react.svg";
+import angular from "./assets/svg/angular.svg";
+import javascript from "./assets/svg/javascript.svg";
+import java from "./assets/svg/java.svg";
+import php from "./assets/svg/php.svg";
+import dataScience from "./assets/svg/data-science.svg";
+import bbdd from "./assets/svg/bbdd.svg";
+import close from "./assets/svg/close.svg";
 type EndPoints = "resources/" | "users/lists" | "roles/";
 
 const END_POINTS = {
@@ -12,5 +20,15 @@ const END_POINTS = {
     post: "roles/" as EndPoints,
   },
 };
-
-export { API_URL, END_POINTS };
+const ICONS = {
+  "Todos": close,
+  "Node": node,
+  "React": react,
+  "Angular": angular,
+  "Javascript": javascript,
+  "Java": java,
+  "Fullstack PHP": php,
+  "Data Science": dataScience,
+  "BBDD": bbdd,
+}
+export { API_URL, END_POINTS, ICONS };
