c1a55f3b205deb6a1bd576f84c155bc7ea16ac41
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 02:16:26 +0100

    refactor: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,24 @@
 import "./assets/css/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import UserCtxProvider from "./context/UserCtxProvider.tsx";
-import GlobalCtxProvider from "./context/GlobalCtxProvider.tsx";
 import { BrowserRouter } from "react-router";
-import { Toaster } from "sonner";
+import App from "./App";
+import UserCtxProvider from "./context/UserCtxProvider";
+import GlobalCtxProvider from "./context/GlobalCtxProvider";
+import CustomToaster from "./components/CustomToaster";
 
-createRoot(document.getElementById("root")!).render(
+const app = createRoot(document.getElementById("root")!)
+
+app.render(
   <GlobalCtxProvider>
     <UserCtxProvider>
       <StrictMode>
         <BrowserRouter>
-          <Toaster
-            richColors
-            toastOptions={{
-              style: {
-                padding: "2rem",
-                fontSize: "1rem",
-              },
-            }}
-          />
+          <CustomToaster />
           <App />
-          <Toaster
-            richColors
-            toastOptions={{
-              style: {
-                padding: "2rem",
-                fontSize: "1rem",
-              },
-            }}
-          />
+          <CustomToaster />
         </BrowserRouter>
       </StrictMode>
     </UserCtxProvider>
-  </GlobalCtxProvider>,
+  </GlobalCtxProvider>
 );
