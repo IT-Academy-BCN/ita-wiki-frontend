7b80c91c3af3fb944a2e907bff7ebc9034b4fe9b
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:32:54 +0100

    featured: Extending interfaces

--- src/context/typesCtx.ts
+++ src/context/typesCtx.ts
@@ -1,8 +1,12 @@
 import { IntUseMainMenu } from "../hooks/useMainMenu";
 import { IntUser } from "../types";
 import { IUseResize } from "../hooks/useResize";
+import { AccessProps } from "../hooks/useAccess";
+import { AccessModalProps } from "../components/access/useAccessModal";
+import { UseUserRol } from "../hooks/useUser";
+import { ModalState } from "../hooks/useModals";
 
-export interface PropsContextUser {
+export interface PropsContextUser extends UseUserRol {
   user: IntUser;
   saveUser: (user: IntUser) => void;
   signIn: () => void;
@@ -10,4 +14,10 @@ export interface PropsContextUser {
   error: string | null;
   setError: (error: string | null) => void;
 }
-export interface PropsContexGLobal extends IUseResize, IntUseMainMenu {}
+export interface PropsContexGLobal extends
+  IUseResize,
+  IntUseMainMenu,
+  AccessProps,
+  AccessModalProps,
+  ModalState { }
+
