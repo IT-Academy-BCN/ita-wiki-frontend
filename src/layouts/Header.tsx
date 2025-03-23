ec6cc79c7cdf79111083b9aa3eaf976b872e64aa
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:54:44 +0100

    refactor: Header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,5 @@
-
-import MainHeader from "../Layout/MainHeader"
-
+import MainHeader from "./MainHeader"
 const Header = () => {
-
   return (
     <header className="flex items-center justify-between sticky top-0 z-50 bg-gray-800 px-4">
       <MainHeader />
