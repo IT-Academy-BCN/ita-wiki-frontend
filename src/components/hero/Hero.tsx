72b33755962ba82dfeb27be27bc15185f664af7e
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:38:09 +0100

    feature: Add Hero

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from "react"
+
+interface HeroProps {
+  children: ReactNode
+}
+
+const Hero: FC<HeroProps> = ({ children }) => {
+  return (
+    <article className="flex flex-col gap-8 items-center justify-center min-h-96">
+      {children}
+    </article>
+  )
+}
+
+export default Hero
