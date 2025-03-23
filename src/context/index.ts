9b97d0d3467828eb32ca13a7d8e9b93f27bb0e27
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 10:59:14 +0100

    refactor: context

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,8 +1,10 @@
 import { createContext } from "react";
-import { PropsContexGLobal, PropsContextUser } from "./typesCtx";
+import {
+  PropsContexGLobal,
+  PropsContextUser,
+} from "./typesCtx";
 
 const CtxUser = createContext<PropsContextUser | null>(null);
-
 const CtxGLobal = createContext<PropsContexGLobal | null>(null);
 
 export { CtxUser, CtxGLobal };
