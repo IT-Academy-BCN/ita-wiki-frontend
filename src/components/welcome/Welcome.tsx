4ef8d84870cb3cd039600d7671024fa283a01e08
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 22:23:36 +0100

    refactor: Welcome

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -9,11 +9,13 @@ type WelcomeProps = {
 const Welcome: React.FC<WelcomeProps> = () => {
   const { user } = useUser();
   return (
-    <section>
-      <h1 className="font-bold">
-        ¡Bienvenido{user && ` ,${user.displayName}`} a la wiki de la IT Academy!
-      </h1>
-      <p>Este es el componente de bienvenida.</p>
+    <section className="flex items-center justify-center h-40 p-4">
+      {user ?
+        <h1 className="text-center font-bold">
+          ¡Bienvenido {user.displayName} a la wiki de la IT Academy!
+        </h1> : <h1 className="font-bold text-center ">
+          ¡Bienvenido a la wiki de la IT Academy!
+        </h1>}
     </section>
   );
 };
