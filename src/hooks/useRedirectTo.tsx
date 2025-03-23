b84fbc8858ef8015e5d140a2cc181370318229f2
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:46:55 +0100

    feature: Create hook useRedirectTo

---
+++ src/hooks/useRedirectTo.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router";
+
+export const useRedirectTo = () => {
+  const navigate = useNavigate();
+  const goTo = (url: string) => {
+    navigate(url);
+  };
+  return {
+    goTo,
+  };
+}
