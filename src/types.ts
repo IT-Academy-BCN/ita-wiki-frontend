88f075cbc895bb1c8f19f55900046a9b6d7c491e
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:17:26 +0100

    feature: Add TypModalKey

--- src/types.ts
+++ src/types.ts
@@ -2,18 +2,20 @@ import { ReactNode } from "react";
 import { categories } from "./data/categories";
 import { themes } from "./data/themes";
 import { resourceTypes } from "./data/resourceTypes";
+
 export type Category = (typeof categories)[number];
 export type Theme = (typeof themes)[number];
 export type ResourceType = (typeof resourceTypes)[number];
-export type TypChildren = {
-  children?: ReactNode;
-};
+export type TypModalKey = "addUser" | "addResource" | "access";
+export type TypChildren = { children?: ReactNode; };
+
 export interface IntUser {
   id: number;
   displayName: string | null;
   photoURL: string | undefined;
   role?: string;
 }
+
 export interface IntResource {
   id?: number;
   github_id: number;
@@ -26,5 +28,4 @@ export interface IntResource {
   theme: Theme;
   type: ResourceType;
 }
-
 
