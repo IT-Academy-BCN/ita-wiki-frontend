6fcad8d6a38c6e40c44896c51cb2aa9de12b5a49
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:46:27 +0100

    feature: Create hook usePermissions

---
+++ src/hooks/usePermisions.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useUserCtx } from "./useUserCtx";
+import { ROLE_PERMISSIONS } from "../config";
+
+export const usePermissions = () => {
+  const { user } = useUserCtx();
+
+  const [userRole, setUserRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (user && user.id) {
+      setUserRole(() => user.role || null);
+    } else {
+      setUserRole(() => null);
+    }
+  }, [user]);
+
+  const hasPermission = userRole
+    ? ROLE_PERMISSIONS.superadmin.includes(userRole)
+    : false;
+
+  return {
+    userRole,
+    hasPermission
+
+  };
+}
