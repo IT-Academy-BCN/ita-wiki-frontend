7fed2c27ab07ac4647d7b625836b79d6cdd48922
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:19:50 +0100

    feature: Add CustomToaster

---
+++ src/components/CustomToaster.tsx
@@ -0,0 +1,19 @@
+import { Toaster } from "sonner";
+const CustomToaster: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <Toaster
+        richColors
+        toastOptions={{
+          style: {
+            padding: "2rem",
+            fontSize: "1rem",
+          },
+        }}
+      />
+      {children}
+    </>
+  )
+}
+
+export default CustomToaster
