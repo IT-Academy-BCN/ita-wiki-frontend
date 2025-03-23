a3eb4fe4797951aaae93171c057ab871e3dcdd50
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:24:24 +0100

    refactor: GlobalCtxProvider

--- src/context/GlobalCtxProvider.tsx
+++ src/context/GlobalCtxProvider.tsx
@@ -1,7 +1,11 @@
 import { FC, ReactNode } from "react";
-import { CtxGLobal, PropsContexGLobal } from ".";
+import { CtxGLobal } from ".";
 import { useResize } from "../hooks/useResize";
 import { useMainMenu } from "../hooks/useMainMenu";
+import { PropsContexGLobal } from "./typesCtx";
+import { useAccess } from "../hooks/useAccess";
+import { useAccessModal } from "../components/access/useAccessModal";
+import { useModals } from "../hooks/useModals";
 
 interface Props {
   children: ReactNode;
@@ -11,6 +15,9 @@ const GlobalCtxProvider: FC<Props> = ({ children }) => {
   const value = {
     ...useResize(),
     ...useMainMenu(),
+    ...useAccess(),
+    ...useAccessModal(),
+    ...useModals()
   } as PropsContexGLobal;
 
   return <CtxGLobal.Provider value={value}>{children}</CtxGLobal.Provider>;
