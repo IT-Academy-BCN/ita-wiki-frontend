4d6b39418c9ce89631b601790e77312e682eaa79
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:37:19 +0100

    feature: Add Welcome

---
+++ src/components/welcome/Welcome.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IntUser } from "../../types";
+import { useUser } from "../../hooks/useUser";
+
+type WelcomeProps = {
+  user?: IntUser;
+};
+
+const Welcome: React.FC<WelcomeProps> = () => {
+  const { user } = useUser();
+  return (
+    <section>
+      <h1 className="font-bold">¡Bienvenido{user && ` ,${user.displayName}`} a la wiki de la IT Academy!</h1>
+      <p>Este es el componente de bienvenida.</p>
+    </section>
+  );
+};
+
+export default Welcome;
