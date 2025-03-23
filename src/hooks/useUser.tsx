3f0e40208c4f8da1f06ee0a3de7b4f2acdd62bf3
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:30:24 +0100

    refactor: useUser

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IntUser } from "../types";
 import { signInWithGitHub } from "../api/firebase";
 import { storage } from "../utils";
 import { getUserRole } from "../api/userApi";
 
+export interface UseUserRol {
+  rol: IntUser | undefined;
+  handleSetRole: () => void;
+}
+
+
 export const useUser = () => {
   const [user, setUser] = useState<IntUser | null>(storage.get("user"));
   const [error, setError] = useState<string | null>(null);
+  const { rol, handleSetRole } = useUserRol({ user });
 
   const signIn = async () => {
     try {
       const newUser = await signInWithGitHub();
-      setUser(newUser);
-      await handleSetRole();
+      setUser(() => newUser);
+      handleSetRole();
     } catch (error) {
-      if (error instanceof Error) {
-        setError(error.message);
+      if (error) {
+        const customError = error as Error;
+        setAccessError(customError.message);
       } else {
-        setError("An unknown error occurred.");
+        setAccessError("Se produjo un error desconocido.");
       }
     }
   };
@@ -25,19 +33,45 @@ export const useUser = () => {
   const signOut = () => {
     localStorage.removeItem("user");
     setUser(null);
-    setError(null);
+    setAccessError(null);
   };
 
   const saveUser = (user: IntUser) => {
     setUser(() => user);
   };
+  const setAccessError = (message: string | null) => {
+    setError(() => message);
+  }
+  useEffect(() => {
+    if (rol) {
+      setUser(() => rol)
+    }
+    handleSetRole()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return {
+    user,
+    rol,
+    saveUser,
+    signIn,
+    signOut,
+    error,
+    setError,
+    handleSetRole,
+    setAccessError
+  };
+};
+
+export const useUserRol = ({ user }: { user: IntUser | null }): UseUserRol => {
+  const [rol, setRol] = useState<IntUser>();
 
   const handleSetRole = async () => {
     if (user) {
       try {
         const userRole = await getUserRole(user.id);
         const updatedUser = { ...user, role: userRole };
-        setUser(updatedUser);
+        setRol(() => updatedUser);
         storage.save("user", updatedUser);
       } catch (error) {
         throw new Error(error as string);
@@ -45,13 +79,5 @@ export const useUser = () => {
     }
   };
 
-  return {
-    user,
-    saveUser,
-    signIn,
-    signOut,
-    error,
-    setError,
-    handleSetRole,
-  };
-};
+  return { rol, handleSetRole }
+}
