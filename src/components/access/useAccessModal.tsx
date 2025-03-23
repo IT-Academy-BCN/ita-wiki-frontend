eb04014f3b91170227ceb8e515f115af6470fb62
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 02:06:53 +0100

    feature: Create useAccessModal

---
+++ src/components/access/useAccessModal.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export interface AccessModalProps {
+  isAccesssModal: boolean;
+  openAccessModal: () => void;
+  closeAccessModal: () => void;
+  toggleAccessModal: () => void;
+}
+
+export const useAccessModal = () => {
+  const [isAccesssModal, setIsAccesssModal] = useState(false);
+  const openAccessModal = () => setIsAccesssModal(() => true);
+  const closeAccessModal = () => setIsAccesssModal(() => false);
+  const toggleAccessModal = () => setIsAccesssModal(() => false);
+
+  return {
+    isAccesssModal,
+    openAccessModal,
+    closeAccessModal,
+    toggleAccessModal
+  } as AccessModalProps
+}
