958e800bb78a7f998131d3329c5f62eda857d686
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:21:45 +0100

    feature: Add Terms

---
+++ src/components/access/Terms.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+import { useGlobalCtx } from "../../hooks/useGlobalCtx"
+
+const Terms: FC = () => {
+  const { isCheckedTerms, handleCheckboxChange } = useGlobalCtx()
+  return (
+    <label htmlFor="terms" className="block mt-8">
+      <input
+        name="terms"
+        id="terms"
+        type="checkbox"
+        onChange={handleCheckboxChange}
+        checked={isCheckedTerms}
+      ></input>
+      Acepto términos legales
+    </label>
+  )
+}
+
+export default Terms
