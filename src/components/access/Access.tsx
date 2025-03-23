c43846d712f4da208a5d2112534b3d5b2192e8ec
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:39:29 +0100

    feature: Add Access

---
+++ src/components/access/Access.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface AccessProps {
+  children?: React.ReactNode;
+}
+
+const Access: React.FC<AccessProps> = ({ children }) => {
+  return <>
+    {children}
+  </>
+};
+
+export default Access;
