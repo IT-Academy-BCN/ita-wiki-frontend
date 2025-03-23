9702fd98ebe963bceb0f9038c9f629bf7f00c2e6
JuanDNJ <juandevnjv@gmail.com>
2025-03-23 01:20:45 +0100

    feature: ErrorAccess

---
+++ src/components/access/ErrorAccess.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+const ErrorAccess: FC<{ loginError: boolean }> = ({ loginError }) => {
+
+  return (
+    loginError && (
+      <div className="text-red-500 text-sm mt-2">
+        <div className="text-red-500 text-sm mt-2 text-center">
+          Lo sentimos, no se ha podido iniciar sesión,
+          <br /> contacte con el administrador.
+        </div>
+      </div>
+    )
+  )
+}
+
+export default ErrorAccess
