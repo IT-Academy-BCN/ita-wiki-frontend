7a45ab44748d77d0594c7b05584e2bbdf0f227c3
Gordana Marjanovic <gogamar@gmail.com>
2025-04-01 18:21:11 +0200

    Fix Resource test

--- src/components/resources/__test__/Resource.test.tsx
+++ src/components/resources/__test__/Resource.test.tsx
@@ -9,16 +9,22 @@ const moockrResources = moock.resources.map(
       ...resource,
       created_at: "2025-02-25 00:00:00",
       updated_at: "2025-02-25 00:00:00",
-    }) as IntResource,
+    }) as IntResource
 );
 
 describe("Resource Component", () => {
   it("should render the component and display the correct title", () => {
-    render(<Resource resource={moockrResources[0]} />);
+    render(
+      <Resource
+        resource={moockrResources[0]}
+        isBookmarked={false}
+        toggleBookmark={() => {}}
+      />
+    );
 
     const titleElement = screen.getByTestId("resource");
     expect(titleElement).toHaveClass(
-      "border-[1px] border-[#7E7E7E] rounded-[20px] w-full lg:max-w-[467.27px]",
+      "border-[1px] border-[#7E7E7E] rounded-[20px] w-full lg:max-w-[467.27px]"
     );
   });
 });
