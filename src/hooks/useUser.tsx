a97b47fe11f94ccf80cf481343cd60044a7914e0
DaniloEspinosa <115655795+DaniloEspinosa@users.noreply.github.com>
2025-07-09 08:58:47 +0200

    refactor: simplify useUser hook

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,58 +1,15 @@
-import { useState } from "react";
-import { IntUser } from "../types";
-import { signInWithGitHub } from "../api/firebase";
 import { useUserContext } from "../context/UserContext";
-import { getUserRole } from "../api/userApi";
 
 export const useUser = () => {
-  const { user, setUser } = useUserContext();
-  const [error, setError] = useState<string | null>(null);
-
-  const signIn = async () => {
-    try {
-      const newUser = await signInWithGitHub();
-      setUser(newUser);
-      await handleSetRole();
-    } catch (error) {
-      if (error instanceof Error) {
-        setError(error.message);
-      } else {
-        setError("An unknown error occurred.");
-      }
-    }
-  };
-
-  const signOut = () => {
-    setUser(null);
-    setError(null);
-  };
-
-  const saveUser = (user: IntUser) => {
-    setUser(user);
-  };
-
-  const handleSetRole = async () => {
-    if (user) {
-      try {
-        const userRole = await getUserRole(user.id);
-        const updatedUser = { ...user, role: userRole };
-        setUser(updatedUser);
-      } catch (error) {
-        throw new Error(error as string);
-      }
-    }
-  };
-
-  const isAuthenticated = !!user;
-
+  const context = useUserContext();
+  
   return {
-    user,
-    isAuthenticated,
-    saveUser,
-    signIn,
-    signOut,
-    error,
-    setError,
-    handleSetRole,
+    user: context.user,
+    isAuthenticated: context.isAuthenticated,
+    saveUser: context.saveUser,
+    signIn: context.signIn,
+    signOut: context.signOut,
+    error: context.error,
+    setError: context.setError,
   };
 };
