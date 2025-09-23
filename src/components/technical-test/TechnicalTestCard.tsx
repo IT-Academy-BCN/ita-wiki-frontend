4cfba7b7eb26cacb1732de1a9d5b0f3a71fb68d9
DaniloEspinosa <115655795+DaniloEspinosa@users.noreply.github.com>
2025-09-23 07:34:54 +0200

    Add the correct icons in technicalTestCard

--- src/components/technical-test/TechnicalTestCard.tsx
+++ src/components/technical-test/TechnicalTestCard.tsx
@@ -1,25 +1,30 @@
 import { TechnicalTest } from "../../types/TechnicalTest";
-import Angular from "../../assets/angular.svg";
-import Java from "../../assets/logo-java 1.svg";
+import XnixCalendar from "../../assets/xnix-calendar.svg";
+import { asideContentForTechnicalTest } from "../Layout/aside/asideContent";
 
 interface TechnicalTestCardProps {
   test: TechnicalTest;
 }
 
 const TechnicalTestCard = ({ test }: TechnicalTestCardProps) => {
+  const language = asideContentForTechnicalTest.find(
+    (item) => item.label === test.language,
+  );
+  const IconComponent = language?.icon as unknown as React.FC<
+    React.SVGProps<SVGSVGElement>
+  >;
   return (
     <div className="flex flex-col w-full py-4 px-2 rounded-2xl shadow-xs border border-[#7E7E7E]">
       <li className="flex justify-between">
         <div className="flex flex-col gap-2 m-2">
           <h3 className="text-lg font-bold">{test.title}</h3>
-          <span className="text-sm text-gray-500">🗓️ 23 Julio 2025</span>
+          <span className="text-sm text-gray-500">
+            <img src={XnixCalendar} alt="XnixCalendar" /> 23 Julio 2025
+          </span>
         </div>
         <div className="flex gap-2 m-2">
           <span>
-            <img src={Angular} alt="Angular" />
-          </span>
-          <span>
-            <img src={Java} alt="Java" />
+            <IconComponent />
           </span>
         </div>
       </li>
