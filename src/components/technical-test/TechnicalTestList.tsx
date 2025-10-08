0d59cfcf25bd471c41d1b16f5e0511aad8913353
AuroraHumo <carles.casallachs@gmail.com>
2025-10-08 11:30:50 +0200

    implemented showLoader logic to TechList

--- src/components/technical-test/TechnicalTestList.tsx
+++ src/components/technical-test/TechnicalTestList.tsx
@@ -1,16 +1,19 @@
 import useTechnicalTests from "../../hooks/useTechnicalTests";
 import TechnicalTestCard from "./TechnicalTestCard";
 import LoadingImage from "../ui/LoadingImage";
+import {useMinLoading} from "../../hooks/useMinLoading";
+
 
 const TechnicalTestList = () => {
   const { technicalTests, isLoading, error } = useTechnicalTests();
+  const showLoader = useMinLoading(isLoading);
 
   return (
     <div className="w-2/3 flex flex-col m-4">
       <h2 className="text-2xl font-bold mb-8">Pruebas técnicas</h2>
-      {isLoading && <LoadingImage text="Cargando pruebas técnicas..." />}
+      {showLoader && <LoadingImage text="Cargando pruebas técnicas..." />}
       {error && <p className="m-4 text-red-500">Error: {error.message}</p>}
-      {!isLoading && !error && (
+      {!showLoader && !error && (
         <ul className="flex flex-col gap-4">
           {technicalTests.map((test) => (
             <TechnicalTestCard key={test.id} test={test} />
