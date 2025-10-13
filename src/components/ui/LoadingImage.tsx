ee9da4a4da7a198dcf3960c7ce4dca82bc54756e
AuroraHumo <carles.casallachs@gmail.com>
2025-10-13 11:58:30 +0200

    Adapt loading image component to Figma design

--- src/components/ui/LoadingImage.tsx
+++ src/components/ui/LoadingImage.tsx
@@ -7,9 +7,12 @@ interface LoadingImageProps {
 
 const LoadingImage: FC<LoadingImageProps> = ({ text }) => {
   return (
-    <div>
-      <img src={loading} alt="Cargando..." className="w-1/2" />
-      <p> {text}</p>
+    <div className="flex flex-col items-center w-150 mt-20">
+      <img src={loading} alt="Cargando..." className="w-1/4" />
+      <h1 className="font-black">{text}</h1>
+      <p className="text-gray-500 text-sm mt-2">
+        Prepárate para poner a prueba tu código y tus habilidades
+      </p>
     </div>
   );
 };
