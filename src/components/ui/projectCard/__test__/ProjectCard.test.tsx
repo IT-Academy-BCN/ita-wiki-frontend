23b4f7c9021e1ec9501223efa95f4fc15a59f0d9
JlBestMc <jlrcmt@gmail.com>
2025-10-27 17:54:24 +0100

    test formatted correctly.

--- src/components/ui/projectCard/__test__/ProjectCard.test.tsx
+++ src/components/ui/projectCard/__test__/ProjectCard.test.tsx
@@ -2,22 +2,25 @@ import { render, screen } from "@testing-library/react";
 import ProjectCard from "../ProjectCard";
 
 describe("ProjectCard", () => {
-	const props = {
-		logoFront: "../assets/react.svg",
-		logoBack: "../assets/logo-php 1.svg",
-		avatarSrc: "../assets/project-avatar1.svg",
-		avatarSrc2: "../assets/project-avatar2.svg",
-		avatarSrc3: "../assets/project-avatar3.svg",
-	};
+  const props = {
+    logoFront: "../assets/react.svg",
+    logoBack: "../assets/logo-php 1.svg",
+    avatarSrc: "../assets/project-avatar1.svg",
+    avatarSrc2: "../assets/project-avatar2.svg",
+    avatarSrc3: "../assets/project-avatar3.svg",
+  };
 
-	test("renders tech sections with logos", () => {
-		render(<ProjectCard {...props} />);
+  test("renders tech sections with logos", () => {
+    render(<ProjectCard {...props} />);
 
-		const imgs = screen.getAllByRole("img");
-		const hasFrontLogo = imgs.some((img) => img.getAttribute("src") === props.logoFront);
-		const hasBackLogo = imgs.some((img) => img.getAttribute("src") === props.logoBack);
-		expect(hasFrontLogo).toBe(true);
-		expect(hasBackLogo).toBe(true);
-	});
+    const imgs = screen.getAllByRole("img");
+    const hasFrontLogo = imgs.some(
+      (img) => img.getAttribute("src") === props.logoFront,
+    );
+    const hasBackLogo = imgs.some(
+      (img) => img.getAttribute("src") === props.logoBack,
+    );
+    expect(hasFrontLogo).toBe(true);
+    expect(hasBackLogo).toBe(true);
+  });
 });
-
