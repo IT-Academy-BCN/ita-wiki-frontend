2a30f7c6481d147f24fe06fbbbe1ece228e5c449
AuroraHumo <carles.casallachs@gmail.com>
2025-10-30 10:54:42 +0100

    refactor(code-connect): moved button outside to apply single responsability and prepare it to be reusable in other pages in future sprints. added filters to main code connect page

--- src/components/code-connect/CodeConnectFiltersComponent.tsx
+++ src/components/code-connect/CodeConnectFiltersComponent.tsx
@@ -1,34 +1,56 @@
+import { FC, SVGProps } from "react";
 import { useState } from "react";
-import { filtersContent, FilterItem } from "./filtersContent";
+import { filtersContent } from "./filtersContent";
 
-export const CodeConnectFiltersComponent = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState<
-    FilterItem["label"] | null
-  >(null);
+type ButtonProps = {
+  label: string;
+  icon: FC<SVGProps<SVGSVGElement>>;
+  isSelected: boolean;
+  handleSelect: (label: string) => void;
+};
 
-  const handleSelect = (label: FilterItem["label"]) => {
+const Button = ({
+  label,
+  icon: Icon,
+  isSelected,
+  handleSelect,
+}: ButtonProps) => {
+  return (
+    <button
+      onClick={() => handleSelect(label)}
+      aria-pressed={isSelected ? "true" : "false"}
+      className={`flex items-center gap-2 px-4 py-2 rounded-lg border-2 transition-all hover:shadow-md
+              ${isSelected ? "border-[#B91879]" : "border-gray-300"} `}
+    >
+      <Icon className="w-5 h-5" />
+      <span className="text-sm font-medium">{label}</span>
+    </button>
+  );
+};
+
+const CodeConnectFiltersComponent = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+
+  const handleSelect = (label: string) => {
     setSelectedLanguage(label);
   };
 
   return (
     <div className="flex flex-wrap gap-3 mb-4">
-      {filtersContent.map(({ icon: Icon, label }) => {
+      {filtersContent.map(({ icon, label }) => {
         const isSelected = selectedLanguage === label;
-
         return (
-          <button
+          <Button
             key={label}
-            onClick={() => handleSelect(label)}
-            aria-pressed={isSelected ? "true" : "false"}
-            className={`flex items-center gap-2 px-4 py-2 rounded-lg border-2 transition-all hover:shadow-md
-              ${isSelected ? "border-[#B91879]" : "border-gray-300"}
-              bg-white text-black`}
-          >
-            <Icon className="w-5 h-5" />
-            <span className="text-sm font-medium">{label}</span>
-          </button>
+            label={label}
+            icon={icon}
+            isSelected={isSelected}
+            handleSelect={handleSelect}
+          />
         );
       })}
     </div>
   );
 };
+
+export default CodeConnectFiltersComponent;
