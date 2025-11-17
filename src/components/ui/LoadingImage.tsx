1e8e83cff15d2949608bf58708b558d049e68bc9
JlBestMc <jlrcmt@gmail.com>
2025-11-17 05:28:43 +0100

    Formatting issue corrected in LoadingImage component

--- src/components/ui/LoadingImage.tsx
+++ src/components/ui/LoadingImage.tsx
@@ -8,7 +8,7 @@ interface LoadingImageProps {
 const LoadingImage: FC<LoadingImageProps> = ({ text }) => {
   return (
     <div className="flex flex-col items-center  mt-20">
-      <img src={loading} alt="Cargando..." className="max-w-xs"/>
+      <img src={loading} alt="Cargando..." className="max-w-xs" />
       <h1 className="font-black">{text}</h1>
       <p className="text-gray-500 text-sm mt-2">
         Prepárate para poner a prueba tu código y tus habilidades
