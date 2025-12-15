a97e1992f2dad29dcfb524400eb2af23d324a361
mstne03 <mserraitorne@gmail.com>
2025-12-15 13:46:32 +0100

    fix: run format

--- src/types.ts
+++ src/types.ts
@@ -8,7 +8,12 @@ export type Category = (typeof categories)[number];
 export type Theme = (typeof themes)[number] | null;
 export type ResourceType = (typeof resourceTypes)[number];
 
-export type TypUserRole = "student" | "mentor" | "admin" | "superadmin" | "anonymous";
+export type TypUserRole =
+  | "student"
+  | "mentor"
+  | "admin"
+  | "superadmin"
+  | "anonymous";
 
 export type TypChildren = {
   children?: ReactNode;
