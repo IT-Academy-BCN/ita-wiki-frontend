ca517d3f762e697171b3761c13d9def935c6dfaa
JlBestMc <jlrcmt@gmail.com>
2025-12-15 04:41:08 +0100

    Skeleton layout added for the TechnicalTestCards

--- src/components/technical-test/TechnicalTestList.tsx
+++ src/components/technical-test/TechnicalTestList.tsx
@@ -1,9 +1,9 @@
 import useTechnicalTests from "../../hooks/useTechnicalTests";
 import TechnicalTestCard from "./TechnicalTestCard";
-import LoadingImage from "../ui/LoadingImage";
 import { useMinLoading } from "../../hooks/useMinLoading";
 import { useNavigate } from "react-router";
 import ButtonComponent from "../atoms/ButtonComponent";
+import TechnicalTestSkeleton from "./TechnicalTestSkeleton";
 
 const TechnicalTestList = () => {
   const { technicalTests, isLoading, error } = useTechnicalTests();
@@ -23,7 +23,13 @@ const TechnicalTestList = () => {
           </ButtonComponent>
         </div>
       </div>
-      {showLoader && <LoadingImage text="Carregant proves tècniques..." />}
+      {showLoader && !error && (
+        <ul className="flex flex-col gap-4">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <TechnicalTestSkeleton key={index} />
+          ))}
+        </ul>
+      )}
       {error && <p className="m-4 text-red-500">Error: {error.message}</p>}
       {!showLoader && !error && (
         <ul className="flex flex-col gap-4">
