bbf49813e5cc664279c8cd92603f33479876680a
mstne03 <mserraitorne@gmail.com>
2025-12-15 10:18:16 +0100

    fix: adds role prop to IntUser interface to solve type safety errors

--- src/components/resources/UserResource.tsx
+++ src/components/resources/UserResource.tsx
@@ -11,14 +11,14 @@ export const UserResource: FC<TypChildren & UserResourceProps> = ({
   return (
     <div data-testid="user-resource" className="flex gap-2 items-center ">
       <img
-        src={user.photoURL}
+        src={undefined}
         alt="User avatar"
         width={28}
         height={28}
         className="rounded-full w-[28px] h-[28px]"
       />
       <h3 className="text-[#808080] font-bold text-[12px]">
-        {user.displayName}
+        {user.name}
       </h3>
       {children}
     </div>
--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,7 @@ export interface IntUser {
   name: string;
   email: string;
   password: string;
+  role?: string;
 }
 
 export interface IntResource {
