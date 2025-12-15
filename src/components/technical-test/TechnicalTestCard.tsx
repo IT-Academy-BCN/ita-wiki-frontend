2cd8f1c423d2a9f5af723dbb56d1432ffb5ea845
JlBestMc <jlrcmt@gmail.com>
2025-12-15 04:09:01 +0100

    new svg files added and hash function modified to ensure consistent level assignment based on title.

--- src/components/technical-test/TechnicalTestCard.tsx
+++ src/components/technical-test/TechnicalTestCard.tsx
@@ -2,12 +2,27 @@ import { TechnicalTest } from "../../types/TechnicalTest";
 import XnixCalendar from "../../assets/xnix-calendar.svg";
 import { asideContentForTechnicalTest } from "../Layout/aside/asideContent";
 import { Link } from "react-router";
-import testLevel from "../../assets/tests-level.svg";
+import testLevel1 from "../../assets/testsLevel.svg";
+import testLevel2 from "../../assets/testsLevel2.svg";
+import testLevel3 from "../../assets/testsLevel3.svg";
 import { Clock, Heart } from "lucide-react";
 
 interface TechnicalTestCardProps {
   test: TechnicalTest;
 }
+
+const getLevelIcon = (title: string) => {
+  const hash = Array.from(title).reduce(
+    (sum, char) => sum + char.charCodeAt(0),
+    0,
+  );
+
+  const level = (hash % 3) + 1;
+
+  if (level === 1) return testLevel1;
+  if (level === 2) return testLevel2;
+  return testLevel3;
+};
 
 const TechnicalTestCard = ({ test }: TechnicalTestCardProps) => {
   const language = asideContentForTechnicalTest.find(
@@ -28,12 +43,14 @@ const TechnicalTestCard = ({ test }: TechnicalTestCardProps) => {
 
   const likeCount = test.like_count ?? 0;
 
+  const levelIcon = getLevelIcon(test.title);
+
   return (
     <li className="flex flex-col w-full py-4 px-4 rounded-2xl shadow-xs border border-[#7E7E7E]">
       <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
         <div className="flex flex-col sm:flex-row gap-1">
           <div>
-            <img src={testLevel} alt="Test level" className="mt-1" />
+            <img src={levelIcon} alt="Test level" className="mt-1" />
           </div>
           <div className="flex flex-col gap-1">
             <h3>
