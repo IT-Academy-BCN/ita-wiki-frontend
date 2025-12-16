17eaaab0e767208d5ea1bdac0cad4ed9e3a5b1f2
mstne03 <mserraitorne@gmail.com>
2025-12-16 12:49:29 +0100

    fix: eliminate unused variables from role dropdown test file

--- src/components/Layout/__test__/RoleDropdownComponent.test.tsx
+++ src/components/Layout/__test__/RoleDropdownComponent.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, test, vi } from "vitest";
 import RoleDropdownComponent from "../header/RoleDropdownComponent";
 import { TypUserRole } from "../../../types";
