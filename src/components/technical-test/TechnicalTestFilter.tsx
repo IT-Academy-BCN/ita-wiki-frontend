c6e2923db4749e14e5c947f4ffb8b99857633e06
ireneruiz00 <ruizsirene2@gmail.com>
2025-12-16 12:50:51 +0100

    resolved conflict with incoming change import

--- src/components/technical-test/TechnicalTestFilter.tsx
+++ src/components/technical-test/TechnicalTestFilter.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, useState } from "react";
-import { asideContentForTechnicalTest } from "../Layout/aside/asideContent";
+import { contentForTechnicalTest } from "./languageLabelsContent";
 import FilterGroup from "../ui/FilterGroup";
 
 interface TechnicalTestFilterProps {
